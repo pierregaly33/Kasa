751c94618745cac391927cb4e8aa116e418d7f54
pierregaly33 <galy.pierre31@gmail.com>
2024-06-20 10:58:23 +0200

    ajout underline navbar et modification transparence banner

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/banner";
 import image from "../assets/img/imageBanner.svg";
 import Location from "../components/location";
+import Navbar from "../components/navbar";
 
 function home() {
     return (
